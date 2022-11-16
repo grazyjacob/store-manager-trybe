f24fa01c407cd4e9f563eff0aa2f7ab2d94c3572
Graziely Jacob <jacobgrazy@gmail.com>
2022-11-16 16:49:45 -0300

    Requisito 4 completo

--- src/routers/products.router.js
+++ src/routers/products.router.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const productsController = require('../controllers/products.controller');
+const validateName = require('../middlewares/validateName');
 
 const router = express.Router();
 
 router.get('/products/:id', productsController.getProductsById);
 router.get('/products', productsController.getProducts);
-router.post('/products', productsController.postProduct);
+router.post('/products', validateName, productsController.postProduct);
 
 module.exports = router;
